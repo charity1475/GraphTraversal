c47db11267e0b6f84128dc17121fd2efe5022e48
Charity <charitymbisi@gmail.com>
2024-12-30 02:49:55 +0300

    added sample and image to readme

--- README.md
+++ README.md
@@ -6,3 +6,21 @@ Weighted, Unweighted, directed and non directed graphs
 - [Breadth First Search]()
 - [Depth First Search]()
 - [Dijkstra's Algorithm]()
+
+## Graphs
+## Stations
+- **Station A**
+- **Station B**
+- **Station C**
+- **Station D**
+- **Station E**
+
+## Edges with Weights
+1. **Station A → Station B**: 5
+2. **Station A → Station C**: 10
+3. **Station B → Station D**: 15
+4. **Station C → Station E**: 20
+5. **Station D → Station E**: 25
+
+## Graph Visualisation
+![img.png](img.png)
---
+++ src/main/java/org/graph/starter/Graph.java
@@ -0,0 +1,56 @@
+package org.graph;
+
+import java.util.ArrayList;
+import org.graph.starter.Vertex;
+
+public class Main {
+  /**
+   * The list of vertices in the graph.
+   */
+  private ArrayList<Vertex> vertices;
+  private boolean weighted;
+  private boolean directed;
+
+  public Main(boolean weighted, boolean directed) {
+    this.vertices = new ArrayList<Vertex>();
+    this.weighted = weighted;
+    this.directed = directed;
+  }
+
+  public Vertex addVertex(String data) {
+    Vertex vertex = new Vertex(data);
+    this.vertices.add(vertex);
+    return vertex;
+  }
+
+  public void addEdge(Vertex startVertex, Vertex endVertex, Integer weight) {
+    if (!this.weighted) {
+      weight = null;
+    }
+    if (this.directed) {
+      startVertex.addEdge(endVertex, weight);
+    } else {
+      startVertex.addEdge(endVertex, weight);
+      endVertex.addEdge(startVertex, weight);
+    }
+  }
+
+  public void removeEdge(Vertex startVertex, Vertex endVertex){
+    startVertex.removeEdge(endVertex);
+    if(!this.directed){
+      endVertex.removeEdge(startVertex);
+    }
+  }
+
+  public void removeVertex(Vertex vertex){
+    this.vertices.remove(vertex);
+    for(Vertex v : this.vertices){
+      v.removeEdge(vertex);
+    }
+  }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
